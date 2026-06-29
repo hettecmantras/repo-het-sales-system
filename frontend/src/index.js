4dd33497be5cbbe89cf57b836fd1e4a624516d02
hettecmantras <het.tecmantras@gmail.com>
2026-06-29 13:39:29 +0530

    Add dashboard and payment routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import App from './App';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
+import Payment from './components/Payment';
 import './App.css';
 
 ReactDOM.render(
@@ -11,6 +13,8 @@ ReactDOM.render(
     <Switch>
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/payment" component={Payment} />
       <Route path="/" component={App} />
     </Switch>
   </Router>,
